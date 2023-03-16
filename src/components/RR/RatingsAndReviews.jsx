cff9533a73d046011495215e0742a20bbfb03e3e
Kyle Stevens <kyleastevens3@gmail.com>
2023-03-16 15:37:51 -0700

    Created first render of Ratings and reviews

---
+++ src/components/RR/RatingsAndReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RatingsAndReviews() {
+  return (
+    <div className="text-green-600">
+      <div>Hello from R&R</div>
+    </div>
+  );
+}
+
+export default RatingsAndReviews;
