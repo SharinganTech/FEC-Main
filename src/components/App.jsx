d345ab1dc6c67f8122db4f8dc4d205b262fd6fc2
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-16 12:07:00 -0700

    Create webpack and setup files

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div class='text-red'>
+      Hello World!
+      hello world #2
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  resolve: { extensions: ['', '.js', '.jsx'] },
+  plugins: [],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
