0a7631ae35e33bbcd14d909f412ebe5db4438880
Andrew Ihn <andrewihn11@gmail.com>
2023-03-16 14:54:19 -0700

    App done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Overview from './Overview';
+import RR from './RR';
+import QA from './QA';
+import RIC from './RIC';
 
 function App() {
   return (
     <div className="text-red-600">
-      Hello World!
+      <Overview />
+      <RR />
+      <QA />
+      <RIC />
     </div>
   );
 }
