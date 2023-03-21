a79ba3970681abb676a5233d525cc143fa4776b1
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-21 10:00:08 -0700

    commit merge changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,26 +20,15 @@ function App() {
     axios
       .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
         headers: {
-<<<<<<< HEAD
-          Authorization: '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',
-=======
           Authorization: process.env.AUTH_TOKEN,
->>>>>>> main
         },
       })
       .then((result) => {
         // console.log('results data', result.data);
         setProductId(result.data[0].id);
-<<<<<<< HEAD
-        setCanRender(true);
-      })
-      .catch((err) => {
-        console.log('err getting list of products: ', err);
-=======
       })
       .catch((err) => {
         throw new Error('Error in getting data', err);
->>>>>>> main
       });
   }, []);
 
@@ -50,15 +39,9 @@ function App() {
   }
   return (
     <ProductContext.Provider value={productId}>
-<<<<<<< HEAD
       <Overview />
       {/* <RelatedItemsAndComparison /> */}
       {/* <QA /> */}
-=======
-      {/* <Overview /> */}
-      {/* <RelatedItemsAndComparison /> */}
-      <QA />
->>>>>>> main
       {/* <RatingsAndReviews /> */}
     </ProductContext.Provider>
   );
