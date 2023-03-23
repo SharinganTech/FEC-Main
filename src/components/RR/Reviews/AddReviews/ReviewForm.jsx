bce8b79a51ba8f754ebc7f8f3bfa06637d21d43e
Kyle Stevens <kyleastevens3@gmail.com>
2023-03-23 15:12:03 -0700

    working on photo pop up

--- src/components/RR/Reviews/AddReviews/ReviewForm.jsx
+++ src/components/RR/Reviews/AddReviews/ReviewForm.jsx
@@ -36,22 +36,22 @@ function ReviewForm({ reviewsMeta }) {
       </div>
       <label htmlFor="summary">
         Summary:
-        <input className="ml-1" name="summary" maxLength="60" placeholder="Example: Best Purchase ever!" defaultValue="" />
+        <input className="ml-1" type="text" name="summary" maxLength="60" placeholder="Example: Best Purchase ever!" defaultValue="" />
       </label>
       <label htmlFor="body">
         Review:
-        <input className="ml-1" name="body" maxLength="1000" placeholder="Why did you like the product or not?" defaultValue="" />
+        <input className="ml-1" type="text" name="body" maxLength="1000" placeholder="Why did you like the product or not?" defaultValue="" />
       </label>
       <button onClick={() => setShowImageUpload(true)} className="text-black border-2 border-black font-bold py-4 px-4 mr-2 rounded" type="button">Upload Photo</button>
       {showImageUpload ? <Modal modalContent={imageUpload} /> : null}
       <label htmlFor="nickname">
         Nickname:
-        <input className="ml-1" name="nickname" maxLength="60" placeholder="Example: jackson11!" defaultValue="" />
+        <input className="ml-1" type="text" name="nickname" maxLength="60" placeholder="Example: jackson11!" defaultValue="" />
       </label>
       <div>For privacy reasons, do not use your full name or email address</div>
       <label htmlFor="email">
         Email:
-        <input className="ml-1" name="email" maxLength="60" placeholder="Example: jackson11@email.com" defaultValue="" />
+        <input className="ml-1" type="text" name="email" maxLength="60" placeholder="Example: jackson11@email.com" defaultValue="" />
       </label>
       <div>For authentication reasons, you will not be emailed</div>
     </form>
--- src/components/RR/Reviews/ReviewImages.jsx
+++ src/components/RR/Reviews/ReviewImages.jsx
@@ -2,29 +2,29 @@ import React, { useState } from 'react';
 import Modal from './Modal';
 
 function ReviewImages({ reviewPhotos }) {
-  const [showFullImage, setShowFullImage] = useState(false);
+  const [fullImage, setFullImage] = useState(false);
 
-  const fullImage = (source) => (
-    <div>
+  const fullImageContent = (source) => (
+    <div className="max-h-[80%]">
       <button
-        onClick={() => setShowFullImage(false)}
-        className="mr-auto text-4xl font-bold"
+        onClick={() => setFullImage('')}
+        className="text-4xl font-bold"
         type="button"
       >
         X
       </button>
-      <img className="w-full h-full object-contain" src={source} alt="Review" />
+      <img className="t-30 w-full max-h-[70%] object-contain" src={source} alt="Review" />
     </div>
   );
 
   return (
     <div className="flex flex-row">
       {reviewPhotos.map((image) => (
-        <div>
-          <button onClick={() => setShowFullImage(true)} type="button">
-            <img className="w-full h-40 object-contain" key={image.id} src={image.url} alt="Review" />
+        <div key={image.id}>
+          <button onClick={() => setFullImage(image.url)} type="button">
+            <img className="w-full h-40 object-contain" src={image.url} alt="Review" />
           </button>
-          {showFullImage ? <Modal modalContent={fullImage(image.url)} /> : null}
+          {fullImage === image.url ? <Modal modalContent={fullImageContent(image.url)} /> : null}
         </div>
       ))}
     </div>
