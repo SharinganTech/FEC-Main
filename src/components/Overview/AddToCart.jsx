d2d5f71ef7b0b06ac96aa375726da1e37ca398fa
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-24 13:32:25 -0700

    Implement functional size and quantity selectors. Add to cart creates and object containing the selected information

--- src/components/Overview/AddToCart.jsx
+++ src/components/Overview/AddToCart.jsx
@@ -1,53 +1,75 @@
 import React, { useState } from 'react';
-import DropDownOptions from './DropDown';
 import { arrayOfQuantities } from './helpers';
 
 function AddToCart({ inventory }) {
   // console.log('addtocart; ', inventory);
   const [currentSize, setCurrentSize] = useState('');
+  const [maxQuantity, setMaxQuantity] = useState(null);
   const [currentQuantity, setCurrentQuantity] = useState(null);
-  const sizeOptions = () => {
-    const inven = Object.values(inventory);
-    return inven.map((size, index) => (
-      (size.quantity !== 0) ? <option key={index} value={size.size}>{size.size}</option> : <option value="- Select -">- Select -</option>
-    ));
-  };
+  const inven = Object.values(inventory);
+
+  const sizeOptions = () => (
+    inven.map((size, index) => (
+      (size.quantity !== 0)
+        ? <option key={index} value={size.size}>{size.size}</option>
+        : <option value="- Select -">- Select -</option>
+    ))
+  );
+
   const quantityOptions = (quantity) => {
-    if (!quantity) {
-      return <option value="pick a size">pick a size</option>;
-    }
-    const inven = Object.values(inventory);
-    // console.log('the size: ', inven);
     const quant = inven.filter((size) => size.quantity === quantity);
-    // console.log(quant[0].quantity);
     const maxCount = quant[0].quantity;
     const arr = arrayOfQuantities(maxCount);
-    console.log('result: ', arr);
     return arr.map((number, index) => (
       <option key={index} value={number}>{number}</option>
     ));
   };
+
   const handleChange = (e) => {
     e.preventDefault();
-    const inven = Object.values(inventory);
     setCurrentSize(e.target.value);
 
     const quant = inven.filter((size) => (size.size === e.target.value));
-    setCurrentQuantity(quant[0].quantity);
+    setMaxQuantity(quant[0].quantity);
+  };
+
+  const handleQuantityChange = (e) => {
+    e.preventDefault();
+    setCurrentQuantity(e.target.value);
+  };
+
+  const handleAddToCart = (e) => {
+    e.preventDefault();
+    if (currentQuantity && currentSize) {
+      const cart = { size: currentSize, quantity: currentQuantity };
+      console.log('my cart: ', cart);
+    }
   };
   return (
     <div className="flex flex-wrap w-[425px]">
-      <select className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[200px] text-center" name={currentSize} id={currentSize} value={currentSize} onChange={handleChange}>
+      <select className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[200px] text-center" id={currentSize} value={currentSize} onChange={handleChange}>
         <option value="">Size</option>
-        {sizeOptions(null)}
+        {sizeOptions()}
       </select>
-      <select className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[125px] text-center" name="Quantity" id="Quantity" value={currentQuantity}>
-        {(!currentQuantity)
+      <select className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[125px] text-center" id={maxQuantity} value={currentQuantity} onChange={handleQuantityChange}>
+        {(!maxQuantity)
           ? <option value="">Quantity</option>
-          : quantityOptions(currentQuantity)}
+          : quantityOptions(maxQuantity)}
       </select>
-      <button className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[125px] text-center" type="button" value="">ADD TO BAG +</button>
-      <button className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[200px] text-center" type="button">ADD TO FAVORITE +</button>
+      <button
+        className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[125px] text-center"
+        type="button"
+        value=""
+        onClick={handleAddToCart}
+      >
+        ADD TO BAG +
+      </button>
+      <button
+        className="boarder-solid border-black border-4 bg-white m-[10px] h-[60px] w-[200px] text-center"
+        type="button"
+      >
+        ADD TO FAVORITE +
+      </button>
     </div>
   );
 }
