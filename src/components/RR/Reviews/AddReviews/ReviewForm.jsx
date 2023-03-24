19c0b3b507ec98b0c46bc2476329c6bc39a4e8d3
Kyle Stevens <kyleastevens3@gmail.com>
2023-03-24 14:08:21 -0700

    working on addreview form

--- src/components/RR/Reviews/AddReviews/ReviewForm.jsx
+++ src/components/RR/Reviews/AddReviews/ReviewForm.jsx
@@ -34,26 +34,38 @@ function ReviewForm({ reviewsMeta }) {
           />
         ))}
       </div>
-      <label htmlFor="summary">
-        Summary:
-        <input className="ml-1" type="text" name="summary" maxLength="60" placeholder="Example: Best Purchase ever!" defaultValue="" />
-      </label>
-      <label htmlFor="body">
-        Review:
-        <input className="ml-1" type="text" name="body" maxLength="1000" placeholder="Why did you like the product or not?" defaultValue="" />
-      </label>
-      <button onClick={() => setShowImageUpload(true)} className="text-black border-2 border-black font-bold py-4 px-4 mr-2 rounded" type="button">Upload Photo</button>
-      {showImageUpload ? <Modal modalContent={imageUpload} /> : null}
-      <label htmlFor="nickname">
-        Nickname:
-        <input className="ml-1" type="text" name="nickname" maxLength="60" placeholder="Example: jackson11!" defaultValue="" />
-      </label>
-      <div>For privacy reasons, do not use your full name or email address</div>
-      <label htmlFor="email">
-        Email:
-        <input className="ml-1" type="text" name="email" maxLength="60" placeholder="Example: jackson11@email.com" defaultValue="" />
-      </label>
-      <div>For authentication reasons, you will not be emailed</div>
+      <div className="flex flex-wrap -mx-3 mb-4">
+        <div className="w-3/4 ml-[12.5%]">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="summary">
+            Summary:
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="summary" maxLength="60" placeholder="Example: Best Purchase ever!" defaultValue="" />
+          </label>
+          <p className="text-red-500 text-xs italic">cond render:Please fill out this field.</p>
+        </div>
+        <div className="w-3/4 ml-[12.5%]">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="body">
+            Review:
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="body" maxLength="1000" placeholder="Why did you like the product or not?" defaultValue="" />
+          </label>
+          <p className="text-red-500 text-xs italic">cond render:Please fill out this field.</p>
+        </div>
+        <button onClick={() => setShowImageUpload(true)} className="text-black border-2 border-black font-bold py-4 px-4 mr-2 rounded" type="button">Upload Photo</button>
+        {showImageUpload ? <Modal modalContent={imageUpload} /> : null}
+        <div className="w-3/4 ml-[12.5%]">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="nickname">
+            Nickname:
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="nickname" maxLength="60" placeholder="Example: jackson11!" defaultValue="" />
+          </label>
+          <p className="text-xs italic">For privacy reasons, do not use your full name or email address</p>
+        </div>
+        <div className="w-3/4 ml-[12.5%]">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
+            Email:
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="email" maxLength="60" placeholder="Example: jackson11@email.com" defaultValue="" />
+          </label>
+          <p className="text-xs italic">For authentication reasons, you will not be emailed</p>
+        </div>
+      </div>
     </form>
   );
 }
