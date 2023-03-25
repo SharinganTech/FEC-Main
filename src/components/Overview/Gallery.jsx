478836bd0d0059bb5cc81432327c22902078ba7e
Nam Nguyen <nam.nguyen201@gmail.com>
2023-03-25 13:57:05 -0700

    add arrows to the gallery thumbnails

--- src/components/Overview/Gallery.jsx
+++ src/components/Overview/Gallery.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faArrowLeft, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function Gallery({ stylePhotos, mainImage, changeMain }) {
   const [activeIndex, setActiveIndex] = useState('');
@@ -17,8 +17,8 @@ function Gallery({ stylePhotos, mainImage, changeMain }) {
   const changeNext = (e) => {
     e.preventDefault();
     const nextImageIndex = Number(e.target.value) + 1;
-    console.log(typeof nextImageIndex, nextImageIndex);
-    console.log(stylePhotos[nextImageIndex]);
+    console.log('next index', typeof nextImageIndex, nextImageIndex);
+    console.log('urls at that index', stylePhotos[nextImageIndex]);
     if (nextImageIndex === stylePhotos.length) {
       setActiveIndex(0);
       changeMain(stylePhotos[0].url);
@@ -31,8 +31,8 @@ function Gallery({ stylePhotos, mainImage, changeMain }) {
   const changePrev = (e) => {
     e.preventDefault();
     const nextImageIndex = Number(e.target.value) - 1;
-    console.log(typeof nextImageIndex, nextImageIndex);
-    console.log(stylePhotos[nextImageIndex]);
+    console.log('prev index', typeof nextImageIndex, nextImageIndex);
+    console.log('prev urls', stylePhotos[nextImageIndex]);
     if (nextImageIndex < 0) {
       setActiveIndex(stylePhotos.length - 1);
       changeMain(stylePhotos[stylePhotos.length - 1].url);
@@ -42,38 +42,61 @@ function Gallery({ stylePhotos, mainImage, changeMain }) {
     }
   };
 
+  const shiftUp = (e) => {
+
+  };
+
+  const shiftDown = (e) => {
+
+  };
+
   return (
     <div className="flex flex-row bg-pastelGray justify-around h-[650px] w-[850px] overflow-hidden">
-      <div className="flex flex-col justify-start items-center overflow-hidden w-[95px] mt-[10px]">
-        {stylePhotos.map((style, index) => (
-          <button className=" border-line border-2 border-black w-[75px] h-[75px] mt-[2.5px] mb-[2.5px]" key={index} id={index} type="button" onClick={clickThumbnail}>
-            <img
-              id={index}
-              key={index}
-              className={(style.url === mainImage)
-                ? 'opacity-100 object-cover w-[73px] h-[73px]'
-                : 'opacity-40 object-cover w-[73px] h-[73px]'}
-              src={style.thumbnail_url}
-              alt=""
-            />
-          </button>
-        ))}
-      </div>
-      <div className="flex flex-row justify-center items-center h-[100%] w-24 z-50">
-        <button type="button" value={activeIndex} className="h-[100%] w-[100%]" onClick={changePrev}>
+      <div id="sideThumbnails">
+        <button type="button" value={activeIndex} className="h-[20px] w-[75px]" onClick={shiftUp}>
           <FontAwesomeIcon
-            icon={faArrowLeft}
-            className="self-center ml-5"
-
+            icon={faArrowUp}
+            className="self-center"
+          />
+        </button>
+        <div className="flex flex-col shrink-0 grow-0 justify-start items-center overflow-hidden w-[95px] h-[600px] hmt-[10px]">
+          {stylePhotos.map((style, index) => (
+            <button className=" border-line border-2 border-black w-[75px] h-[75px] mt-[2.5px] mb-[2.5px]" key={index} id={index} type="button" onClick={clickThumbnail}>
+              <img
+                id={index}
+                key={index}
+                className={(style.url === mainImage)
+                  ? 'opacity-100 object-cover w-[73px] h-[73px]'
+                  : 'opacity-40 object-cover w-[73px] h-[73px]'}
+                src={style.thumbnail_url}
+                alt=""
+              />
+            </button>
+          ))}
+        </div>
+        <button type="button" value={activeIndex} className="h-[20px] w-[75px]" onClick={shiftDown}>
+          <FontAwesomeIcon
+            icon={faArrowDown}
+            className="self-center"
           />
         </button>
       </div>
-      <img className="object-contain h-[98%] w-[98%]" src={mainImage} alt="" />
-      <div className="flex justify-start h-[100%%] w-24 z-50">
+      <div className="flex shrink-0 flex-row grow-0 justify-center items-center h-[100%] w-16 z-50">
+        <button type="button" value={activeIndex} className="h-[100%] w-[100%]" onClick={changePrev}>
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            className="self-center"
+          />
+        </button>
+      </div>
+      <div className="flex items-center justify-center grow-0 w-[600px] h-[650px]">
+        <img className="object-contain w-[98%] h-[98%]" src={mainImage} alt="" />
+      </div>
+      <div className="flex shrink-0 grow-0 justify-start h-[100%] w-16 z-50">
         <button type="button" value={activeIndex} className="h-[100%] w-[100%]" onClick={changeNext}>
           <FontAwesomeIcon
             icon={faArrowRight}
-            className="self-center mr-5"
+            className="self-center"
           />
         </button>
       </div>
