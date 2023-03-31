d6c1bb6f0516768965c8ac4206f6a327e79fcb5b
Kyle Stevens <kyleastevens3@gmail.com>
2023-03-31 10:51:14 -0700

    starting to refactor context

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,3 +1,7 @@
-import { createContext } from 'react';
+import React, { createContext, useState, useMemo } from 'react';
 
-export default createContext(null);
+export const ProductContext = createContext(null);
+
+export const ProductProvider(({ children })) {
+  const [reviewsMeta, setReviewsMeta] =
+}
