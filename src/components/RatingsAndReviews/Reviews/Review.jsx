c4d28055f2f8626908047b047048e46107a1e255
Kyle Stevens <kyleastevens3@gmail.com>
2023-03-31 10:31:34 -0700

    css changes to review

--- src/components/RatingsAndReviews/Reviews/Review.jsx
+++ src/components/RatingsAndReviews/Reviews/Review.jsx
@@ -7,29 +7,27 @@ function Review({ review, makeGetRequest }) {
   const date = new Date(review.date);
   return (
     <div className="flex flex-col">
-      <div className="hover:shadow-inner">
-        <div>
-          <div className="text-[#798EA4] text-sm">
-            <Stars rating={review.rating} numReviews={1} />
-          </div>
-          <div className="text-s float-right">
-            {review.reviewer_name}
-            {', '}
-            {date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
-          </div>
+      <div className="flex flex-row justify-between">
+        <div className="text-[#798EA4] text-sm">
+          <Stars rating={review.rating} numReviews={1} />
         </div>
-        <ReviewBody
-          reviewSummary={review.summary}
-          reviewBody={review.body}
-          reviewPhotos={review.photos}
-          recommended={review.recommend}
-        />
-        <HelpfulButtons
-          reviewID={review.review_id}
-          helpfulness={review.helpfulness}
-          makeGetRequest={makeGetRequest}
-        />
+        <div className="text-s text-gray-500">
+          {review.reviewer_name}
+          {', '}
+          {date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
+        </div>
       </div>
+      <ReviewBody
+        reviewSummary={review.summary}
+        reviewBody={review.body}
+        reviewPhotos={review.photos}
+        recommended={review.recommend}
+      />
+      <HelpfulButtons
+        reviewID={review.review_id}
+        helpfulness={review.helpfulness}
+        makeGetRequest={makeGetRequest}
+      />
       <hr className="h-0.5 bg-black mt-4 mb-5" />
     </div>
   );
