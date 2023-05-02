c49c48dceed6292a5f1ba9cab827154939400394
Aidan Fisher <121323175+aidanFisher97@users.noreply.github.com>
2023-05-01 19:18:52 -0700

    Update Stars.jsx

--- src/components/RIC/Stars.jsx
+++ src/components/RIC/Stars.jsx
@@ -5,9 +5,7 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { generatePartialStar } from './HelperFunctions';
 
 function Stars({ rating, numReviews, color }) {
-  // console.log('rating', rating);
   const fullStars = Math.floor(rating);
-  // console.log('fullstars', fullStars);
   let halfStars;
   // if the rating minus the fullStars is less then 0.25, there should be no partial stars
   if (rating - fullStars < 0.25) {
